f82d762309054ec2ffb8569c7e1afb6c7dd03af4
Abdelouahed Ouarrar <ouarrar.abdelouahed@gmail.com>
2025-01-21 08:02:35 +0100

    controllers n courseroutes

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
 /* ROUTE IMPORTS */
+import courseRoutes from "./routes/courseRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -28,6 +29,8 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hello World");
   });
+
+app.use("/courses", courseRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
