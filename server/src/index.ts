752058e31ce5cec6f916cc297337665a0017afb6
Abdelouahed Ouarrar <ouarrar.abdelouahed@gmail.com>
2025-01-21 22:14:20 +0100

    userClerk controller n Routes setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,10 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import { createClerkClient } from "@clerk/express";
 /* ROUTE IMPORTS */
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -15,6 +17,10 @@ const isProduction = process.env.NODE_ENV === "production";
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
@@ -27,10 +33,11 @@ app.use(cors());
 
 /* ROUTES */
 app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
+  res.send("Hello World");
+});
 
 app.use("/courses", courseRoutes);
+app.use("/users/clerk", userClerkRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
