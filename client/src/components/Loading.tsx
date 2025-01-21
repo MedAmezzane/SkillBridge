f63f94ae1101ac2ed660e7511e292453a17dbfca
Med.AMEZZANE <med.amezzane@gmail.com>
2025-01-21 14:42:49 +0100

    Accordion Sections component

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,10 +1,10 @@
-import { Loader2 } from "lucide-react";
+import { LoaderCircle } from "lucide-react";
 import React from "react";
 
 const Loading = () => {
   return (
     <div className="loading">
-      <Loader2 className="loading__spinner" />
+      <LoaderCircle className="loading__spinner" />
       <span className="loading__text">Loading...</span>
     </div>
   );
