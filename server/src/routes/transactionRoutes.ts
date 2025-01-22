01a7cb02faee30ee4ee56d64120210343187ad98
Abdelouahed Ouarrar <ouarrar.abdelouahed@gmail.com>
2025-01-22 13:24:10 +0100

    stripe setup controllers routes n api

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,14 +1,8 @@
 import express from "express";
-import {
-  createStripePaymentIntent,
-  createTransaction,
-  listTransactions,
-} from "../controllers/transactionController";
+import { createStripePaymentIntent } from "../controllers/transactionController";
 
 const router = express.Router();
 
-router.get("/", listTransactions);
-router.post("/", createTransaction);
 router.post("/stripe/payment-intent", createStripePaymentIntent);
 
 export default router;
